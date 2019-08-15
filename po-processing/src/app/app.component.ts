c241af1c863eaaabda0c80a839f26006779e118b
David Thong Nguyen <dev@davidtn.com>
2019-08-15 14:05:39 -0700

    Can tally up quantity available

--- po-processing/src/app/app.component.ts
+++ po-processing/src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { Lot } from './lot';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'po-processing';
-  warehouseInput: any;
+  warehouseInput = '';
+  warehouseTotalAvailable = 0;
+  purchaseOrderInput = '';
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  parseWarehouseData() {
+    if (this.warehouseInput.trim() === '') {
+      return null;
+    }
+    this.warehouseTotalAvailable = 0;
+    const lotsData = this.warehouseInput.split('\n');
+    const result = [];
+
+    for (const l of lotsData) {
+      const rawLotData = l.split('\t');
+      const newLot = new Lot({
+        lotId: rawLotData[2],
+        itemSku: rawLotData[0],
+        availableQuant: parseInt(rawLotData[7], 10) || 0
+      });
+
+      this.warehouseTotalAvailable += newLot.availableQuant;
+      result.push(newLot);
+    }
+    return result;
+  }
 }
--- po-processing/src/app/lot.ts
+++ po-processing/src/app/lot.ts
@@ -1,12 +1,25 @@
+/**
+ * Item Code
+ * Description
+ * Whse Lot #
+ * Receipt Date
+ * Customer Lot
+ * Whse Lot Reference
+ * Pallet Id
+ * Qty Available
+ * Qty On Hand
+ * Weight On Hand
+ * Cube On Hand
+ */
 export class Lot {
-  lotId: string;
-  itemSku: string;
-  availableQuant: number;
+  lotId: string; // 2
+  itemSku: string; // 0
+  availableQuant: number; // 7
 
   constructor(fields?: {
-    lotId: string;
-    itemSku: string;
-    availableQuant: number;
+    lotId?: string;
+    itemSku?: string;
+    availableQuant?: number;
   }) {
     if (fields) {
       Object.assign(this, fields);
