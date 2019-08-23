ecff0d0bc3eb7bf422bc5d8357ef20a938a4aa8d
David Thong Nguyen <dev@davidtn.com>
2019-08-23 16:26:09 -0700

    Housing keeping

--- po-processing/src/app/app.module.ts
+++ po-processing/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WarehouseFormComponent } from './components/warehouse-form/warehouse-form.component';
-import { PoFormComponent } from './components/po-form/po-form.component';
 
 import * as Sentry from '@sentry/browser';
 import { CommonFormComponent } from './components/common-form/common-form.component';
 import { ClickableContentComponent } from './components/clickable-content/clickable-content.component';
 import { UploadExcelComponent } from './components/upload-excel/upload-excel.component';
+// TODO: fix error not logging; use known setup
 Sentry.init({
   dsn: 'https://07d41a48a53b4db484fdcf0d31ac78cc@sentry.io/1540295',
   integrations(integrations) {
@@ -32,8 +31,6 @@ export class SentryErrorHandler implements ErrorHandler {
 @NgModule({
   declarations: [
     AppComponent,
-    WarehouseFormComponent,
-    PoFormComponent,
     CommonFormComponent,
     ClickableContentComponent,
     UploadExcelComponent
