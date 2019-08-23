d3d4cf300f1fb289e5b3d48900e6ea57b02fbea1
David Thong Nguyen <dev@davidtn.com>
2019-08-23 11:51:21 -0700

    Added sentry

--- po-processing/src/app/app.module.ts
+++ po-processing/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injectable, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +7,24 @@ import { AppComponent } from './app.component';
 import { WarehouseFormComponent } from './components/warehouse-form/warehouse-form.component';
 import { PoFormComponent } from './components/po-form/po-form.component';
 
+import * as Sentry from '@sentry/browser';
+Sentry.init({
+  dsn: 'https://07d41a48a53b4db484fdcf0d31ac78cc@sentry.io/1540295'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
+
 @NgModule({
   declarations: [AppComponent, WarehouseFormComponent, PoFormComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
